c966cd14fa5646d4b7179a64bc220f938f63e299
PRITESH GIT <pritesh.19b131135@abes.ac.in>
2023-10-15 19:50:15 +0530

    fixed service

--- controllers/services.js
+++ controllers/services.js
@@ -1,19 +1,21 @@
 const Services = require("../database/service_model")
 
-async function services_api(req,res){
-    
-    try{
-        const category = req.query   
-        const query_object = {}    
-        if(category) query_object.category= { $regex : category, $options : "i"}
+async function services_api(req, res) {
 
-        let api_data = await Services.find(query_object)
-        res.status(200).json({api_data})
+  try {
+    const { category } = req.query
+    const query_object = {}
+    if (category) {
+      query_object.category = { $regex: new RegExp(`^${category}`, "i") };
     }
-    catch(err){
-        res.status(404).send(`API data can not be fetched due to ${err}`)
-    }
-    
+
+    let api_data = await Services.find(query_object)
+    res.status(200).json({ api_data })
+  }
+  catch (err) {
+    res.status(404).send(`API data can not be fetched due to ${err}`)
+  }
+
 }
 
 
