cdf52ccd55ae2df01df5f29bcd052165b336a6c1
PRITESH GIT <pritesh.19b131135@abes.ac.in>
2024-01-21 19:54:51 +0530

    npm build update

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 let atlas='mongodb+srv://connecturbanspace:mf0iulH0anMrR4tQ@cluster0.ovesjpc.mongodb.net/Cluster0?retryWrites=true&w=majority'
 let compass = 'mongodb://127.0.0.1:27017/Urban_Clients'
-mongoose.connect(`${atlas}` ,{useNewUrlParser:true, useUnifiedTopology:true})
+mongoose.connect(`${compass}` ,{useNewUrlParser:true, useUnifiedTopology:true})
 .then(()=>{ 
     console.log("Connection to Database Successfull")})
 .catch((err)=>{ 
